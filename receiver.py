adc394415d9d3ac51b2a4255763e9aa0de972c0e
cnegiar <cnegiar@stanford.edu>
2013-04-23 15:00:34 -0700

    wrote the functions for transmitter and one of the functions for receiver

---
+++ receiver.py
@@ -0,0 +1,118 @@
+import sys
+import math
+import numpy
+import scipy.cluster.vq
+import common_txrx as common
+from numpy import linalg as LA
+import receiver_mil3
+
+class Receiver:
+    def __init__(self, carrier_freq, samplerate, spb):
+        '''
+        The physical-layer receive function, which processes the
+        received samples by detecting the preamble and then
+        demodulating the samples from the start of the preamble 
+        sequence. Returns the sequence of received bits (after
+        demapping)
+        '''
+        self.fc = carrier_freq
+        self.samplerate = samplerate
+        self.spb = spb 
+        print 'Receiver: '
+
+    def detect_threshold(self, demod_samples):
+        '''
+        Calls the detect_threshold function in another module.
+        No need to touch this.
+        ''' 
+        return receiver_mil3.detect_threshold(demod_samples)
+ 
+    def detect_preamble(self, demod_samples, thresh, one):
+        '''
+        Find the sample corresp. to the first reliable bit "1"; this step 
+        is crucial to a proper and correct synchronization w/ the xmitter.
+        '''
+
+        '''
+        First, find the first sample index where you detect energy based on the
+        moving average method described in the milestone 2 description.
+        '''
+        # Fill in your implementation of the high-energy check procedure
+
+        energy_offset = -1 
+        for offset in range(len(demod_samples)):
+             curr_samples= samples[offset: offset+self.spb]
+             average = 0
+             #IGNORE FIRST SPB/4 AND LAST SPB/4 BITS OF SAMPLE 
+             central_samples= curr_samples[(self.spb/4) : (self.spb*3)/4 ]
+             for sample in central_samples:
+                average += sample
+             average/= len(central_samples)
+             #if this average is greater than average of mean_one and threshold, then this use this offset
+             if average > ((one + threshold)/2):
+                energy_offset=offset
+                break 
+
+        if energy_offset < 0:
+            print '*** ERROR: Could not detect any ones (so no preamble). ***'
+            print '\tIncrease volume / turn on mic?'
+            print '\tOr is there some other synchronization bug? ***'
+            sys.exit(1)
+
+        '''
+        Then, starting from the demod_samples[offset], find the sample index where
+        the cross-correlation between the signal samples and the preamble 
+        samples is the highest. 
+        '''
+        # Fill in your implementation of the cross-correlation check procedure
+        
+        preamble_offset = 0
+        preamble_bits[1, 0, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1]
+        preamble_samples=[]
+        for bit in preamble_bits:
+            for x in range(self.spb):
+                if bit==0:
+                    preamble_samples.append(0)
+                else:
+                    preamble_samples.append(one)
+
+        correlation=0
+        largest = 0
+        preamble_offset= 0 
+
+        #Calculate the correlation between preamble and demodulated bits in order to find preamble start in demod_bits
+        for k in range (energy_offset,len(demod_samples)):
+            for x in range (len (preamble_samples)):
+                correlation+= (preamble_samples[x]* demod_samples[x+k]
+            if correlation > largest : 
+                preamble_offset= k 
+
+        
+        '''
+        [preamble_offset] is the additional amount of offset starting from [offset],
+        (not a absolute index reference by [0]). 
+        Note that the final return value is [offset + pre_offset]
+        '''
+
+        return energy_offset + preamble_offset
+        
+    #Need to do this one 
+    def demap_and_check(self, demod_samples, preamble_start):
+        '''
+        Demap the demod_samples (starting from [preamble_start]) into bits.
+        1. Calculate the average values of midpoints of each [spb] samples
+           and match it with the known preamble bit values.
+        2. Use the average values and bit values of the preamble samples from (1)
+           to calculate the new [thresh], [one], [zero]
+        3. Demap the average values from (1) with the new three values from (2)
+        4. Check whether the first [preamble_length] bits of (3) are equal to
+           the preamble. If it is proceed, if not terminate the program. 
+        Output is the array of data_bits (bits without preamble)
+        '''
+
+        # Fill in your implementation
+
+        return data_bits # without preamble
+
+    def demodulate(self, samples):
+        return common.demodulate(self.fc, self.samplerate, samples)
---
+++ transmitter.py
@@ -0,0 +1,54 @@
+import math
+import common_txrx as common
+import numpy
+
+class Transmitter:
+    def __init__(self, carrier_freq, samplerate, one, spb, silence):
+        self.fc = carrier_freq  # in cycles per sec, i.e., Hz
+        self.samplerate = samplerate
+        self.one = one
+        self.spb = spb
+        self.silence = silence
+        print 'Transmitter: '
+    def add_preamble(self, databits):
+        '''
+        Prepend the array of source bits with silence bits and preamble bits
+        The recommended preamble bits is 
+        [1, 0, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1]
+        The output should be the concatenation of arrays of
+            [silence bits], [preamble bits], and [databits]
+        '''
+        databits_with_preamble=[]
+        for x in range (self.silence):
+            databits_with_preamble.append(0)
+        preamble_bits = [1, 0, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1]
+        for bit in preamble_bits:
+            databits_with_preamble.append(bit)
+        for bit in databits:
+            databits_with_preamble.append(bit)
+
+        return databits_with_preamble
+
+
+    def bits_to_samples(self, databits_with_preamble):
+        '''
+        Convert each bits into [spb] samples. 
+        Sample values for bit '1', '0' should be [one], 0 respectively.
+        Output should be an array of samples.
+        '''
+        samples=[]
+        for bit in databits_with_preamble:
+            if bit==0:
+                for x in range (self.spb):
+                    samples.append(0)
+            else: 
+                for x in range (self.spb):
+                    samples.append(self.one)
+        return samples
+        
+
+    def modulate(self, samples):
+        '''
+        Calls modulation function. No need to touch it.
+        '''
+        return common.modulate(self.fc, self.samplerate, samples)
